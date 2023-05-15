f833f6e04869cdd572948c853bbdaf452495e39d
Yoshinori05 <133228728+Yoshinori05@users.noreply.github.com>
2023-05-15 19:46:59 +0800

    Update Cannon.java

--- Cannon.java
+++ Cannon.java
@@ -1,5 +1,9 @@
 /**
-GameCanvas class .....
+Cannon class extends the "GameObject" class and represents a cannon object in a game. 
+The constructor initializes the cannon object with an x-coordinate, a y-coordinate, and an object type. 
+The class has a tileY property, an initHitbox method for setting the hitbox properties of the object, an 
+update method, and a getter method for the tileY property. The update method checks if the object needs 
+animation and updates the animation tick.
 @author Jervie S. Manabat (223961), Giuliana Patricia Gabriele L. Bautista (220811)
 @version May 15,2023
 **/
