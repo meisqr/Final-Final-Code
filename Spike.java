d46eadb3e9f4fa93c254c9c00788e89e1a741485
Yoshinori05 <133228728+Yoshinori05@users.noreply.github.com>
2023-05-15 19:27:28 +0800

    Update Spike.java

--- Spike.java
+++ Spike.java
@@ -1,4 +1,19 @@
-
+/**
+GameCanvas class .....
+@author Jervie S. Manabat (223961), Giuliana Patricia Gabriele L. Bautista (220811)
+@version May 15,2023
+**/
+/*
+I have not discussed the Java language code in my program
+with anyone other than my instructor or the teaching assistants
+assigned to this course.
+I have not used Java language code obtained from another student,
+or any other unauthorized source, either modified or unmodified.
+If any Java language code or documentation used in my program
+was obtained from another source, such as a textbook or website,
+that has been clearly noted with a proper citation in the comments
+of my program.
+*/
 
 public class Spike extends GameObject{
 
